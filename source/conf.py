2119a718de9d3096849f5e8b2db72054986e1444
Rui Guerra <rp.guerra@fct.unl.pt>
2026-06-26 10:29:30 +0100

    gitpages build

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ author = ''
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx_copybutton"]
+extensions = ["sphinx_copybutton","sphinx.ext.githubpages"]
 
 templates_path = ['_templates']
 exclude_patterns = []
